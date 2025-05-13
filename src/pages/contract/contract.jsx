29897ea1d39affb1b988bbdcde5338a48603dda9
ThallesSorrilha <thallessorrilha@gmail.com>
2025-05-13 15:06:42 -0300

    feature(front-end): adding form style

--- src/pages/contract/contract.jsx
+++ src/pages/contract/contract.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import './contract.scss';
 
 const RegisterContract = () => {
   return (
     <div className="container">
-        <div className="list-container">
-            <p>Lista de clientes</p>
-        </div>
-        <main className="form">
-            <h1>Olá</h1>
-        </main>
+      <div className="list-container">
+        <p>Lista de clientes</p>
+      </div>
+      <main className="form">
+        <h1>Olá</h1>
+      </main>
     </div>
   );
 };
