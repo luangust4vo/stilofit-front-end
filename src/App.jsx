5cb720cf6e1c6539818785f72cc1752e5df68f1e
ThallesSorrilha <thallessorrilha@gmail.com>
2025-05-13 13:57:44 -0300

    feature(front-end): adicionando estilo, resolvendo rotas

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/register-contracts' element={<RegisterContractrContract/>} />
+        <Route path='/register-contract' element={<RegisterContract/>} />
       </Routes>
     </Router>
   )
--- src/pages/contract/contract.jsx
+++ src/pages/contract/contract.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const RegisterContract = () => {
   return (
     <div className="container">
+        <h1>Estou na página Register Contract</h1>
     </div>
   );
 };
