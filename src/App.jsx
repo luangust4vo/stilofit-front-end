1969c4ca13b070674b3083a7b97f87bba3933320
ThallesSorrilha <thallessorrilha@gmail.com>
2025-05-13 13:36:02 -0300

    feature(front-end): adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/cadastro-contrato' element={<RegisterContract/>} />
+      </Routes>
+    </Router>
   )
 }
 
