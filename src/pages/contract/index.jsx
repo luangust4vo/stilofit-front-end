ede74bd71009cbe82dcf8cb01e5184bee755cb27
ThallesSorrilha <thallessorrilha@gmail.com>
2025-05-21 23:51:18 -0300

    feature(front-end): completando interface do usuário para cadastro de contratos

--- src/pages/contract/index.jsx
+++ src/pages/contract/index.jsx
@@ -20,44 +20,21 @@ const RegisterContract = () => {
 
   const { handleSubmit, setValue, watch } = methods;
 
-  const contract = watch();
+  const installments = watch("installments");
+  const typeExpire = watch("typeExpire");
+
+  let expireLabel = "Qtde. de ";
+  if (typeExpire === "porSecao") {
+    expireLabel += "Seções";
+  } else if (typeExpire === "porPeriodo") {
+    expireLabel += "Períodos";
+  }
 
   // pensar se precisa
   const [editableFields, setEditableFields] = useState({
-    address: true,
-    district: true,
-    city: true,
-    state: true,
+    //atribute: true,
   });
 
-  // pensar se precisa desse método
-  const searchAddress = async () => {
-    const cep = client.cep?.replace(/\D/g, "");
-    if (cep?.length === 8) {
-      const data = await fetchAddressByCEP(cep);
-      if (data) {
-        setValue("address", data.logradouro || "");
-        setValue("district", data.bairro || "");
-        setValue("city", data.localidade || "");
-        setValue("state", data.uf || "");
-        setEditableFields({
-          address: false,
-          district: false,
-          city: false,
-          state: false,
-        });
-      } else {
-        setEditableFields({
-          address: true,
-          district: true,
-          city: true,
-          state: true,
-        });
-      }
-    }
-  };
-
-  // OK
   const onSubmit = (data) => {
     const contracts = JSON.parse(localStorage.getItem("contratos")) || [];
     const newContract = { ...data, id: crypto.randomUUID() };
@@ -76,12 +53,17 @@ const RegisterContract = () => {
         <FormProvider {...methods}>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="block">
-              <h3>Dados do Contrato</h3>
-              <Input label="Nome" name="name" required />
+              <h3>Dados Gerais do Contrato</h3>
+              <Input label="Nome do Contrato" name="name" required />
               <Select label="Status" name="saleStatus">
                 <option value="">Selecione</option>
                 <option value="Solteiro">Disponível</option>
                 <option value="Casado">Não Disponível</option>
+              </Select>
+              <Select label="Template" name="template">
+                <option value="">Selecione</option>
+                <option value="option1">Opção 1</option>
+                <option value="option2">Opção 2</option>
               </Select>
             </div>
 
@@ -92,15 +74,13 @@ const RegisterContract = () => {
                 <option value="parcelavel">Parcelável</option>
                 <option value="aVista">À vista</option>
               </Select>
-              <Select label="Nº de parcelas" name="months">
-                <option value="">Nº de parcelas</option>
-                <option value="x2">2</option>
-                <option value="x4">4</option>
-                <option value="x6">6</option>
-                <option value="x8">8</option>
-                <option value="x10">10</option>
-                <option value="x12">12</option>
-              </Select>
+              <Input
+                label="Nº de parcelas"
+                name="nInstallments"
+                type="number"
+                min={0}
+                disabled={installments === "aVista"}
+              />
               <NumericFormat
                 name="installmentsValue"
                 customInput={Input}
@@ -113,6 +93,7 @@ const RegisterContract = () => {
                 fixedDecimalScale
                 allowLeadingZeros={false}
                 allowNegative={false}
+                disabled={installments === "aVista"}
               />
               <NumericFormat
                 name="totalValue"
@@ -132,133 +113,89 @@ const RegisterContract = () => {
 
             <div className="block">
               <h3>Vencimento</h3>
-            </div>
-
-            <div className="block">
-              <h3>Atividades</h3>
-            </div>
-
-            <div className="block">
-              <h3>Turma e Horário</h3>
-            </div>
-
-            {/*
-            <div className="block">
-              <h3>Dados do Contrato</h3>
-              <Input label="Nome" name="name" required />
-              <Input label="Email" name="email" />
-              <MaskedInput
-                label="Data de Nascimento"
-                name="birthDate"
-                mask="00/00/0000"
-                required
-              />
-              <Select label="Sexo" name="gender" required>
+              <Select label="Tipo de Vencimento" name="typeExpire">
                 <option value="">Selecione</option>
-                <option value="M">Masculino</option>
-                <option value="F">Feminino</option>
+                <option value="porSecao">por Seção</option>
+                <option value="porPeriodo">por Período</option>
               </Select>
-              <MaskedInput
-                label="CPF"
-                name="cpf"
-                mask="000.000.000-00"
-                required
+              <Input
+                label={expireLabel}
+                name="expire"
+                type="number"
+                min={0}
+                disabled={typeExpire === ""}
               />
-              <Input label="RG" name="rg" />
-              <Select label="Estado Civil" name="maritalStatus">
-                <option value="">Selecione</option>
-                <option value="Solteiro">Solteiro</option>
-                <option value="Casado">Casado</option>
-                <option value="Divorciado">Divorciado</option>
-                <option value="Viúvo">Viúvo</option>
-                <option value="Separado">Separado</option>
+            </div>
+
+            <div className="block">
+              <h3>Turma</h3>
+              <Select label="Turma" name="class">
+                <option value="">Escolha a Turma</option>
+                <option value="class1">Turma 1</option>
+                <option value="class2">Turma 2</option>
               </Select>
-              <MaskedInput
-                label="Vencimento Exame Médico"
-                name="medicalExamDueDate"
-                mask="00/00/0000"
-              />
+              <Input label="Horário" name="time" type="time" required />
+
+              {/* Vou criar um componente */}
+              <h4>Dias da Semana</h4>
+              <label>
+                <input
+                  type="checkbox"
+                  value="domingo"
+                  {...methods.register("weekdays")}
+                />
+                Dom
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="segunda"
+                  {...methods.register("weekdays")}
+                />
+                Seg
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="terca"
+                  {...methods.register("weekdays")}
+                />
+                Ter
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="quarta"
+                  {...methods.register("weekdays")}
+                />
+                Qua
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="quinta"
+                  {...methods.register("weekdays")}
+                />
+                Qui
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="sexta"
+                  {...methods.register("weekdays")}
+                />
+                Sex
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="sabado"
+                  {...methods.register("weekdays")}
+                />
+                Sáb
+              </label>
             </div>
 
-            <div className="block">
-              <h3>Responsável pelo cliente</h3>
-              <Input label="Nome" name="guardianName" />
-              <MaskedInput
-                label="CPF"
-                name="guardianCpf"
-                mask="000.000.000-00"
-              />
-              <MaskedInput
-                label="Telefone"
-                name="guardianPhone"
-                mask="(00) 00000-0000"
-              />
-            </div>
-
-            <div className="block">
-              <h3>Dados de emergência</h3>
-              <Input label="Nome do Contato" name="emergencyContact" />
-              <MaskedInput
-                label="Telefone"
-                name="emergencyPhone"
-                mask="(00) 00000-0000"
-              />
-              <Textarea label="Observações" name="notes" />
-            </div>
-
-            <div className="block">
-              <h3>Dados de contato</h3>
-              <Input label="Email" name="email_contact" />
-              <MaskedInput
-                label="Celular"
-                name="cellphone"
-                mask="(00) 00000-0000"
-              />
-            </div>
-
-            <div className="block">
-              <h3>Dados de Residência</h3>
-              <Select label="Tipo de Residência" name="residenceType">
-                <option value="">Selecione</option>
-                <option value="Residential">Residencial</option>
-                <option value="Commercial">Comercial</option>
-              </Select>
-              <MaskedInput
-                label="CEP"
-                name="cep"
-                mask="00000-000"
-                onBlur={searchAddress}
-              />
-              <Input
-                label="Endereço"
-                name="address"
-                disabled={!editableFields.address}
-              />
-              <Input
-                label="Bairro"
-                name="district"
-                disabled={!editableFields.district}
-              />
-              <Input
-                label="Cidade"
-                name="city"
-                disabled={!editableFields.city}
-              />
-              <Input label="UF" name="state" disabled={!editableFields.state} />
-              <Input label="Número" name="number" />
-              <Input label="Complemento" name="complement" />
-            </div>
-
-            <div className="block">
-              <h3>Informações Adicionais</h3>
-              <Textarea label="Informações adicionais" name="additionalInfo" />
-            </div>
-
-            <div className="block">
-              <h3>Responsabilidade</h3>
-              <Input label="Consultor" name="consultant" />
-            </div>
-            */}
             <Button>Salvar</Button>
           </form>
         </FormProvider>
