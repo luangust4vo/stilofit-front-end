d45badad4ec54ee92a5e31ee717eeef9e2fdae16
ThallesSorrilha <thallessorrilha@gmail.com>
2025-06-13 18:06:23 -0300

    feat(Login): estrutura básica com scss

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,20 @@
+import xLogo from "../../assets/x.png"; // ou então public/assets
+import "./styles.scss";
+
 export default function Login() {
   return (
-    <div>
-      <p>Login</p>
+    <div className="login-wrapper">
+      <div className="background"></div>
+      <div className="login-container">
+        <div className="login-box">
+          <img src={xLogo} alt="logo" />
+          <form className="form">
+            <input className="input" type="email" placeholder="Email"/>
+            <input className="input" type="password" placeholder="Senha"/>
+            <button className="button" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
