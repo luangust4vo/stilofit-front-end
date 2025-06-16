84ee1b6e789a807eb8c6d0bcc65cc4940768edcb
ThallesSorrilha <thallessorrilha@gmail.com>
2025-06-16 17:04:12 -0300

    feat(Login): detecção de Caps Lock

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,17 @@
+import { validationSchemaLogin } from "../../utils/validation";
 import xLogo from "../../assets/x.png"; // ou então public/assets
 import "./styles.scss";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import useKeyPressed from "../../hooks/useKeyPressed";
 
 export default function Login() {
+  const capsLockAtivo = useKeyPressed("CapsLock");
+
+  const methods = useForm({
+    resolver: yupResolver(validationSchemaLogin),
+  });
+
   return (
     <div className="login-wrapper">
       <div className="background"></div>
@@ -9,9 +19,16 @@ export default function Login() {
         <div className="login-box">
           <img src={xLogo} alt="logo" />
           <form className="form">
-            <input className="input" placeholder="Email"/>
-            <input className="input" placeholder="Senha"/>
-            <button className="button" type="submit">Entrar</button>
+            <input className="input" placeholder="Email" />
+            <input className="input" placeholder="Senha" type="password" />
+            <div className="div-caps">
+              {capsLockAtivo ? (
+                <p className="msg-caps">CapsLock ATIVADO</p>
+              ) : (
+                <></>
+              )}
+            </div>
+            <button className="button">Entrar</button>
           </form>
         </div>
       </div>
