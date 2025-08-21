23bb5c5af52b71f9825993d9573d3f3a1b725bba
ThallesSorrilha <thallessorrilha@gmail.com>
2025-08-21 16:18:43 -0300

    chore(funcionarios): adicionando estrutura de arquivos, baseado na de contratos

---
+++ src/pages/employess/components/EmployeeTable.jsx
@@ -0,0 +1,110 @@
+import { useNavigate } from "react-router-dom";
+import Table from "../../../components/Table/Table";
+import { goRegistration, goEdit } from "../../../components/Table/Table";
+import { GenericContextProvider } from "../../../contexts/GenericContext";
+import InfoEmployee from "../info/InfoEmployee.jsx";
+import { Button, LayoutMenu } from "../../../components";
+
+import "./style.scss";
+
+function EmployeeTable() {
+  const navigate = useNavigate();
+  const routeName = "funcionario";
+
+  return (
+    <LayoutMenu>
+      <GenericContextProvider lSName="funcionarios">
+        <Table
+          headerComponent={({ search, setSearch }) => (
+            <>
+              <div className="header-left"></div>
+              <div className="header-right">
+                <input
+                  className="field-search"
+                  placeholder="Buscar..."
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <i className="bi bi-funnel-fill"></i>
+                <Button
+                  className="btn-icon-table"
+                  onClick={() => goRegistration(navigate, routeName)}
+                >
+                  Criar Contrato
+                  <i className="bi-plus"></i>
+                </Button>
+              </div>
+            </>
+          )}
+          headerCells={[
+            "Nome",
+            "Valor Total",
+            "Parcelas",
+            "Valor da Parcela",
+            "Vencimento",
+            "",
+          ]}
+          getRowProps={({ element, setSelectedId }) => ({
+            onClick: () => {
+              setSelectedId(element.id);
+            },
+            style: { cursor: "pointer" },
+          })}
+          visualize={({ selectedId, setSelectedId }) =>
+            selectedId !== null && (
+              <div className="center-modal-overlay">
+                <div className="center-modal-content">
+                  <InfoEmployee
+                    id={selectedId}
+                    onClose={() => setSelectedId(null)}
+                  />
+                </div>
+              </div>
+            )
+          }
+        >
+          {(element) => (
+            <>
+              <td>{element.name}</td>
+              <td>
+                {"R$ " +
+                  Number(element.totalValue).toFixed(2).replace(".", ",")}
+              </td>
+              <td>{element.installments ? element.installments : "-"}</td>
+              <td>
+                {element.installmentsValue
+                  ? "R$ " +
+                    Number(element.installmentsValue)
+                      .toFixed(2)
+                      .replace(".", ",")
+                  : "-"}
+              </td>
+              <td>
+                {element.expire}
+                {element.typeExpire === "por Seção"
+                  ? " aulas"
+                  : element.typeExpire === "por Tempo"
+                  ? " meses"
+                  : ""}
+              </td>
+              <td className="buttons">
+                <Button
+                  className="btn-icon-edit"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    goEdit(navigate, routeName, element.id);
+                  }}
+                  title="Editar"
+                >
+                  <i className="bi bi-pencil-fill bi-cell"></i>
+                </Button>
+              </td>
+            </>
+          )}
+        </Table>
+      </GenericContextProvider>
+    </LayoutMenu>
+  );
+}
+
+export default EmployeeTable;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,6 +14,7 @@ const AppRoutes = () => {
         {CheckoutRoutes()}
         {ClassRoutes()}
         {LoginRoutes()}
+        {EmployeeRoutes()}
       </Routes>
     </BrowserRouter>
   );
