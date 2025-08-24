90d77861ff42a90fdb1fd82bd84b038d1746d5c7
ThallesSorrilha <thallessorrilha@gmail.com>
2025-08-23 21:52:34 -0300

    refact(ContractRoutes, ContractTable): adaptando à nova componentização

--- src/routes/ContractRoutes.jsx
+++ src/routes/ContractRoutes.jsx
@@ -1,4 +1,5 @@
 import { Route } from "react-router-dom";
+import { GenericContextProvider } from "../contexts/GenericContext.jsx";
 import ContractTable from "../pages/contract/components/ContractTable";
 import EditContract from "../pages/contract/Register/EditContract";
 import RegisterContract from "../pages/contract/Register";
@@ -6,7 +7,14 @@ import RegisterContract from "../pages/contract/Register";
 const ContractRoutes = () => {
   return (
     <Route path="/contrato">
-      <Route index element={<ContractTable />} />
+      <Route
+        index
+        element={
+          <GenericContextProvider lSName="contratos">
+            <ContractTable />
+          </GenericContextProvider>
+        }
+      />
       <Route path=":id/editar" element={<EditContract />} />
       <Route path="novo" element={<RegisterContract />} />
     </Route>
