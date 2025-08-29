a7a18fcbcfefa1bf4bc32728285e3221b3fd4ad7
vinicius <viniciusoliveira12353@gmail.com>
2025-08-29 20:04:44 -0300

    Style(Conflito): Foi resolvido o conflito no nome de variaveis do scss

--- src/routes/EmployeeRoutes.jsx
+++ src/routes/EmployeeRoutes.jsx
@@ -3,7 +3,7 @@ import { GenericContextProvider } from "../contexts/GenericContext.jsx";
 import { LayoutMenu } from "../components/index.jsx";
 import EmployeeTable from "../pages/employee/components/EmployeeTable";
 import InfoEmployee from "../pages/employee/info/InfoEmployee";
-import EmployeeEdit from "../pages/employee/Register/EmployeeEdit";
+// import EmployeeEdit from "../pages/employee/Register/EmployeeEdit";
 import RegisterEmployee from "../pages/employee/Register";
 
 const EmployeeRoutes = () => {
@@ -20,7 +20,7 @@ const EmployeeRoutes = () => {
     >
       <Route index element={<EmployeeTable />} />
       <Route path=":id/" element={<InfoEmployee />} />
-      <Route path=":id/editar" element={<EmployeeEdit />} />
+      {/* <Route path=":id/editar" element={<EmployeeEdit />} /> */}
       <Route path="novo" element={<RegisterEmployee />} />
     </Route>
   );
