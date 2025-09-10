7dff7e253e76cf800e0f79270e97261aa0ba95b5
larybino <binolaryssa@gmail.com>
2025-09-09 22:37:18 -0300

    style: ajusta estilo do forms

--- src/pages/client/Register/index.jsx
+++ src/pages/client/Register/index.jsx
@@ -96,108 +96,129 @@ const Register = ({ initialData = null, onSubmit: externalSubmit }) => {
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="block">
               <h3>Dados do Cliente</h3>
-              <Input label="Nome" name="name" required />
-              <Input label="Email" name="email" />
-              <MaskedInput
-                label="Data de Nascimento"
-                name="birthDate"
-                mask="00/00/0000"
-                required
-              />
-              <Select label="Sexo" name="gender" required>
-                <option value="">Selecione</option>
-                <option value="MASCULINO">Masculino</option>
-                <option value="FEMININO">Feminino</option>
-              </Select>
-              <MaskedInput
-                label="CPF"
-                name="cpf"
-                mask="000.000.000-00"
-                required
-              />
-              <MaskedInput label="RG" name="rg" />
-              <Select label="Estado Civil" name="maritalStatus">
-                <option value="">Selecione</option>
-                <option value="SOLTEIRO">Solteiro</option>
-                <option value="CASADO">Casado</option>
-                <option value="DIVORCIADO">Divorciado</option>
-                <option value="VIÚVO">Viúvo</option>
-                <option value="SEPARADO">Separado</option>
-              </Select>
-              <MaskedInput
-                label="Vencimento Exame Médico"
-                name="medicalExamDueDate"
-                mask="00/00/0000"
-              />
+              <div className="row">
+                <Input label="Nome" name="name" required />
+                <Input label="Email" name="email" />
+              </div>
+              <div className="row">
+                <MaskedInput
+                  label="Data de Nascimento"
+                  name="birthDate"
+                  mask="00/00/0000"
+                  required
+                />
+                <Select label="Sexo" name="gender" required>
+                  <option value="">Selecione</option>
+                  <option value="MASCULINO">Masculino</option>
+                  <option value="FEMININO">Feminino</option>
+                </Select>
+                <MaskedInput
+                  label="CPF"
+                  name="cpf"
+                  mask="000.000.000-00"
+                  required
+                />
+                <MaskedInput label="RG" name="rg" />
+              </div>
+              <div className="row">
+                <Select label="Estado Civil" name="maritalStatus">
+                  <option value="">Selecione</option>
+                  <option value="SOLTEIRO">Solteiro</option>
+                  <option value="CASADO">Casado</option>
+                  <option value="DIVORCIADO">Divorciado</option>
+                  <option value="VIÚVO">Viúvo</option>
+                  <option value="SEPARADO">Separado</option>
+                </Select>
+                <MaskedInput
+                  label="Vencimento Exame Médico"
+                  name="medicalExamDueDate"
+                  mask="00/00/0000"
+                />
+              </div>
             </div>
 
             <div className="block">
               <h3>Responsável pelo cliente</h3>
-              <Input label="Nome" name="guardianName" />
-              <MaskedInput
-                label="CPF"
-                name="guardianCpf"
-                mask="000.000.000-00"
-              />
-              <MaskedInput
-                label="Telefone"
-                name="guardianPhone"
-                mask="(00) 00000-0000"
-              />
+              <div className="row">
+                <Input label="Nome" name="guardianName" />
+                <MaskedInput
+                  label="CPF"
+                  name="guardianCpf"
+                  mask="000.000.000-00"
+                />
+                <MaskedInput
+                  label="Telefone"
+                  name="guardianPhone"
+                  mask="(00) 00000-0000"
+                />
+              </div>
             </div>
 
             <div className="block">
               <h3>Dados de emergência</h3>
-              <Input label="Nome do Contato" name="emergencyContact" />
-              <MaskedInput
-                label="Telefone"
-                name="emergencyPhone"
-                mask="(00) 00000-0000"
-              />
+              <div className="row">
+                <Input label="Nome do Contato" name="emergencyContact" />
+                <MaskedInput
+                  label="Telefone"
+                  name="emergencyPhone"
+                  mask="(00) 00000-0000"
+                />
+              </div>
+
               <Textarea label="Observações" name="notes" />
             </div>
 
             <div className="block">
               <h3>Dados de contato</h3>
-              <Input label="Email" name="email_contact" />
-              <MaskedInput
-                label="Celular"
-                name="cellphone"
-                mask="(00) 00000-0000"
-              />
+              <div className="row">
+                <Input label="Email" name="email_contact" />
+                <MaskedInput
+                  label="Celular"
+                  name="cellphone"
+                  mask="(00) 00000-0000"
+                />
+              </div>
             </div>
 
             <div className="block">
               <h3>Dados de Residência</h3>
-              <Select label="Tipo de Residência" name="residenceType">
-                <option value="">Selecione</option>
-                <option value="RESIDENCIAL">Residencial</option>
-                <option value="COMERCIAL">Comercial</option>
-              </Select>
-              <MaskedInput
-                label="CEP"
-                name="cep"
-                mask="00000-000"
-                onBlur={searchAddress}
-              />
-              <Input
-                label="Endereço"
-                name="address"
-                disabled={!editableFields.address}
-              />
-              <Input
-                label="Bairro"
-                name="district"
-                disabled={!editableFields.district}
-              />
-              <Input
-                label="Cidade"
-                name="city"
-                disabled={!editableFields.city}
-              />
-              <Input label="UF" name="state" disabled={!editableFields.state} />
-              <Input label="Número" name="number" />
-              <Input label="Complemento" name="complement" />
+              <div className="row">
+                <Select label="Tipo de Residência" name="residenceType">
+                  <option value="">Selecione</option>
+                  <option value="RESIDENCIAL">Residencial</option>
+                  <option value="COMERCIAL">Comercial</option>
+                </Select>
+                <MaskedInput
+                  label="CEP"
+                  name="cep"
+                  mask="00000-000"
+                  onBlur={searchAddress}
+                />
+                <Input
+                  label="Endereço"
+                  name="address"
+                  disabled={!editableFields.address}
+                />
+                <Input
+                  label="Bairro"
+                  name="district"
+                  disabled={!editableFields.district}
+                />
+              </div>
+              <div className="row">
+                <Input
+                  label="Cidade"
+                  name="city"
+                  disabled={!editableFields.city}
+                />
+                <Input
+                  label="UF"
+                  name="state"
+                  disabled={!editableFields.state}
+                />
+                <Input label="Número" name="number" />
+                <Input label="Complemento" name="complement" />
+              </div>
             </div>
 
             <div className="block">
