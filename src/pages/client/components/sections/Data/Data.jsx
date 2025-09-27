2896c25291a6da12ba2c8041136fd41dab239e48
ThallesSorrilha <thallessorrilha@gmail.com>
2025-09-27 19:39:44 -0300

    chore(data/Data.jsx): adicionando seção de dados do usuário, baseada no código já existente de InfoClient

---
+++ src/pages/client/components/sections/Data/Data.jsx
@@ -0,0 +1,98 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../../components";
+import { goEdit } from "../../../../../utils/navigation.js";
+
+import "./Data.scss";
+
+const Data = ({ selectedClient }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="box-info">
+        {selectedClient ? (
+          <>
+            <p>
+              <strong>Nome:</strong> {selectedClient.name}
+            </p>
+            <p>
+              <strong>Email:</strong>{" "}
+              {selectedClient.email ? selectedClient.email : "-"}
+            </p>
+            <p>
+              <strong>Telefone:</strong>{" "}
+              {selectedClient.cellphone ? selectedClient.cellphone : "-"}
+            </p>
+            <p>
+              <strong>Data de nascimento:</strong> {selectedClient.birthDate}
+            </p>
+            <p>
+              <strong>CPF:</strong> {selectedClient.cpf}
+            </p>
+            <p>
+              <strong>Endereço:</strong>
+              {selectedClient &&
+              [
+                selectedClient.address,
+                selectedClient.number,
+                selectedClient.complement,
+                selectedClient.district,
+                selectedClient.city,
+                selectedClient.state,
+              ]
+                .filter(Boolean)
+                .join(", ").length > 0
+                ? [
+                    selectedClient.address,
+                    selectedClient.number,
+                    selectedClient.complement,
+                    selectedClient.district,
+                    selectedClient.city,
+                    selectedClient.state,
+                  ]
+                    .filter(Boolean)
+                    .join(", ")
+                : " - "}
+            </p>
+          </>
+        ) : (
+          "Dados"
+        )}
+      </div>
+      <div className="box-info">
+        {selectedClient ? (
+          <>
+            <p>
+              <strong>Contrato:</strong>{" "}
+              {selectedClient.contrato ? selectedClient.contrato : "-"}
+            </p>
+          </>
+        ) : (
+          "Informações do contrato"
+        )}
+      </div>
+      <div className="box-info">
+        {selectedClient ? (
+          <>
+            <p>
+              <strong>Observações:</strong>{" "}
+              {selectedClient.additionalInfo
+                ? selectedClient.additionalInfo
+                : "-"}
+            </p>
+          </>
+        ) : (
+          "Campo de texto de observações"
+        )}
+      </div>
+
+      <div className="edit">
+        <Button onClick={goEdit(navigate, "cliente", selectedClient.id)}>
+          Editar
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Data;
