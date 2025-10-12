97c604a6192e95157c6e4013365421d68788e112
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-12 15:34:45 -0300

    feature(Payment.jsx): criando classe de pagamento apenas para testar o fluxo das telas e dos dados referentes

---
+++ src/pages/client/components/sections/Payment/Payment.jsx
@@ -0,0 +1,39 @@
+import { useState, useMemo, useEffect } from "react";
+import SaleService from "../../../../../services/SaleService";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Payment = ({ clientId, saleId }) => {
+  const saleService = useMemo(() => new SaleService(), []);
+  const [fetchedSale, setFetchedSale] = useState(null);
+
+  useEffect(() => {
+    if (saleId && clientId) {
+      const fetchSaleAndValidate = async () => {
+        setFetchedSale(null);
+        setValidationMessage(null);
+        try {
+          const sale = await saleService.findById(saleId);
+          if (sale) {
+            if (sale.clientId === clientId) {
+              setFetchedSale(sale);
+              console.log("Venda e Cliente Validados:", sale);
+            } else {
+              toast.error("Erro de validação do cliente.");
+            }
+          } else {
+            toast.error("Venda não encontrada.");
+          }
+        } catch (error) {
+          console.error("Erro ao buscar venda:", error);
+          toast.error("Erro de comunicação.");
+        }
+      };
+      fetchSaleAndValidate();
+    }
+  }, [saleId, clientId, saleService]);
+
+  return <></>;
+};
+
+export default Payment;
