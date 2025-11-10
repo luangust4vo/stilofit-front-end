9c64478f7c77f65f7c4642189d1d674cc6f8a393
ThallesSorrilha <thallessorrilha@gmail.com>
2025-11-10 20:20:42 -0300

    feature(findByName em PromotionService): adicionando método para encontrar registros por nome

--- src/services/PromotionService.js
+++ src/services/PromotionService.js
@@ -1,14 +1,33 @@
 import BaseService from "./BaseService";
 
-class PromotionService extends BaseService{
-    constructor(){
-        super('/promotions')
-    }
+class PromotionService extends BaseService {
+  constructor() {
+    super("/promotions");
+  }
 
-    async findAll(){
-        const response = await this.api.get(`${this.endPoint}/list-all`);
-        return response.data.content;
-    }
+  async findAll() {
+    const response = await this.api.get(`${this.endPoint}/list-all`);
+    return response.data.content;
+  }
+
+  async findPaginated(page, size = 30) {
+    const params = { page, size };
+    const response = await this.api.get(`${this.endPoint}/list-all`, {
+      params,
+    });
+    return response.data;
+  }
+
+  async findByName(page, name, size = 30) {
+    const params = { page, name, size };
+    const response = await this.api.get(
+      `${this.endPoint}/find-by-name`,
+      {
+        params,
+      }
+    );
+    return response.data;
+  }
 }
 
 export default PromotionService;
