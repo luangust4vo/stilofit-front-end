e9a5a4e9f4190ee478d21d7301113a7e65a7b868
ThallesSorrilha <thallessorrilha@gmail.com>
2025-11-16 14:28:21 -0300

    fix(SaleService > findAll): corrigindo endpoint do método de acordo com o backend

--- src/services/SaleService.js
+++ src/services/SaleService.js
@@ -1,20 +1,15 @@
 import BaseService from "./BaseService";
 
-class SaleService extends BaseService{
-    constructor(){
-        super('/sales')
-    }
+class SaleService extends BaseService {
+  constructor() {
+    super("/sales");
+  }
 
-    async findAll(){
-        const response = await this.api.get(`${this.endPoint}/list-all-sales`);
-        return response.data.content;
-    }
-
-    async findPaginated(page, size = 30) {
-        const params = { page, size };
-        const response = await this.api.get(`${this.endPoint}/list-all-sales`, { params });
-        return response.data; 
-    }
+  async findAll(page, size = 30) {
+    const params = { page, size };
+    const response = await this.api.get(`${this.endPoint}`, { params });
+    return response.data;
+  }
 }
 
 export default SaleService;
